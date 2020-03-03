508c8cae2a82725c5f3388433b0e10fbd7b800a9
Jeffrey Kelling <j.kelling@hzdr.de>
2020-03-03 11:27:54 +0100

    OpenACC (WIP)

--- include/alpaka/alpaka.hpp
+++ include/alpaka/alpaka.hpp
@@ -25,6 +25,7 @@
 #include <alpaka/acc/AccCpuOmp2Blocks.hpp>
 #include <alpaka/acc/AccCpuOmp2Threads.hpp>
 #include <alpaka/acc/AccOmp5.hpp>
+#include <alpaka/acc/AccCpuOacc.hpp>
 #include <alpaka/acc/AccGpuUniformCudaHipRt.hpp>
 #include <alpaka/acc/AccGpuCudaRt.hpp>
 #include <alpaka/acc/AccGpuHipRt.hpp>
@@ -98,6 +99,7 @@
 #include <alpaka/event/EventUniformCudaHipRt.hpp>
 #include <alpaka/event/EventCpu.hpp>
 #include <alpaka/event/EventOmp5.hpp>
+#include <alpaka/event/EventOacc.hpp>
 #include <alpaka/event/Traits.hpp>
 //-----------------------------------------------------------------------------
 // extent
@@ -123,6 +125,7 @@
 #include <alpaka/kernel/TaskKernelCpuOmp2Blocks.hpp>
 #include <alpaka/kernel/TaskKernelCpuOmp2Threads.hpp>
 #include <alpaka/kernel/TaskKernelOmp5.hpp>
+#include <alpaka/kernel/TaskKernelCpuOacc.hpp>
 #include <alpaka/kernel/TaskKernelGpuUniformCudaHipRt.hpp>
 #include <alpaka/kernel/Traits.hpp>
 //-----------------------------------------------------------------------------
@@ -138,6 +141,7 @@
 #include <alpaka/mem/buf/BufCpu.hpp>
 #include <alpaka/mem/buf/BufUniformCudaHipRt.hpp>
 #include <alpaka/mem/buf/BufOmp5.hpp>
+#include <alpaka/mem/buf/BufOacc.hpp>
 #include <alpaka/mem/buf/Traits.hpp>
 
 #include <alpaka/mem/view/ViewCompileTimeArray.hpp>
@@ -172,6 +176,7 @@
 #include <alpaka/pltf/PltfCpu.hpp>
 #include <alpaka/pltf/PltfUniformCudaHipRt.hpp>
 #include <alpaka/pltf/PltfOmp5.hpp>
+#include <alpaka/pltf/PltfOacc.hpp>
 #include <alpaka/pltf/Traits.hpp>
 //-----------------------------------------------------------------------------
 // rand
@@ -187,6 +192,7 @@
 #include <alpaka/queue/QueueCpuNonBlocking.hpp>
 #include <alpaka/queue/QueueCpuBlocking.hpp>
 #include <alpaka/queue/QueueOmp5Blocking.hpp>
+#include <alpaka/queue/QueueOaccBlocking.hpp>
 #include <alpaka/queue/Traits.hpp>
 #include <alpaka/queue/Properties.hpp>
 //-----------------------------------------------------------------------------
