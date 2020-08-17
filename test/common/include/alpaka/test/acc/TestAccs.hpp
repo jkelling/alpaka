34241460cfe6c4edfd9d04574dd654eeef260255
Jeffrey Kelling <j.kelling@hzdr.de>
2020-08-17 16:41:08 +0200

    Disable tests with multiple object files for Oacc+GCC10

--- test/common/include/alpaka/test/acc/TestAccs.hpp
+++ test/common/include/alpaka/test/acc/TestAccs.hpp
@@ -130,17 +130,27 @@ namespace alpaka
                 typename TIdx>
             using AccOmp5IfAvailableElseInt = int;
 #endif
-#if defined(ALPAKA_ACC_ANY_BT_OACC_ENABLED) && !defined(ALPAKA_CUDA_CI) && !(defined(TEST_UNIT_KERNEL_KERNEL_STD_FUNCTION)) \
-        && !(defined(TEST_UNIT_ATOMIC))
-                template<
-                    typename TDim,
-                    typename TIdx>
-                using AccOaccIfAvailableElseInt = alpaka::ctx::CtxThreadOacc<TDim, TIdx>;
+#if defined(ALPAKA_ACC_ANY_BT_OACC_ENABLED) && !(defined(TEST_UNIT_KERNEL_KERNEL_STD_FUNCTION)) \
+    && !( BOOST_COMP_GNUC && ( \
+            /* tests excluded because of GCC10 Oacc / Omp5 target symbol bug with multiple units */ \
+            defined(TEST_UNIT_BLOCK_SHARED) \
+            || defined(TEST_UNIT_BLOCK_SYNC) \
+            || defined(TEST_UNIT_WARP) \
+            || defined(TEST_UNIT_INTRINSIC) \
+            || defined(TEST_UNIT_KERNEL) \
+            || defined(TEST_UNIT_MEM_VIEW) \
+            || defined(TEST_UNIT_MATH) /* because of static const members */ \
+            || defined(TEST_UNIT_MEM_BUF) /* actually works, but hangs when ran by ctest */ \
+        ))
+            template<
+                typename TDim,
+                typename TIdx>
+            using AccOaccIfAvailableElseInt = alpaka::ctx::CtxThreadOacc<TDim, TIdx>;
 #else
-                template<
-                    typename TDim,
-                    typename TIdx>
-                using AccOaccIfAvailableElseInt = int;
+            template<
+                typename TDim,
+                typename TIdx>
+            using AccOaccIfAvailableElseInt = int;
 #endif
 #if (defined(ALPAKA_ACC_GPU_CUDA_ENABLED) && BOOST_LANG_CUDA) || (defined(ALPAKA_ACC_GPU_HIP_ENABLED) && BOOST_LANG_HIP)
             template<
